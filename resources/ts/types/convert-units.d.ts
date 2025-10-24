90d7c35596577e18019125e5bfdae3936f59ce2c
Blackshain <blackshainorg@gmail.com>
2025-10-24 17:10:03 -0400

    boo

--- resources/ts/types/convert-units.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'convert-units' {
-  type Unit = string;
-  interface Converter {
-    from(fromUnit: Unit): { to(toUnit: Unit): number };
-    to(toUnit: Unit): number;
-  }
-  function convert(value: number): Converter;
-  export default convert;
-}
